abb92d3dd898cbc23096407a38443ba7b30e3f5d
Rahulnerdy <111075975+Rahulnerdy@users.noreply.github.com>
2022-09-12 20:13:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,17 @@ const mainContainer = document.querySelector(".main");
 const thankyouContainer = document.querySelector(".thankyou");
 const submitButton = document.getElementById("submit-rating");
 const ratings = document.querySelectorAll(".btn");
-const textRating = document.querySelector("#rating");
+const textRating = document.getElementById("rating");
+
+let ratingsbtns = Array.from(ratings);
 
 submitButton.addEventListener('click', () => {
     mainContainer.style.display = "none"
     thankyouContainer.classList.remove("hidden")
 })
 
-
-ratings.forEach((buttons) => {
-    buttons.addEventListener('click', changeRating)
-    
-})
-
-function changeRating() {
-    ratings.innerHTML = textRating.innerHTML
+for (const btn of ratingsbtns) {
+    btn.addEventListener('click', ()=> {
+        textRating.innerHTML = btn.innerHTML ;
+    });
 }
